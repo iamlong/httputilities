1a1ab20b517537ce057ab437c55a5bd57483d930
Steve Zhang <zhming@microsoft.com>
2018-03-17 07:45:49 +0800

    now all promise works. Lateron, I need to clean up code

--- filedownloader.js
+++ filedownloader.js
@@ -7,8 +7,8 @@ class FileDownloader {
                 var httpclient = require('urllib');
                 httpclient.request(url, function (err, data, res) {
                     if (err) {
-                        console.log('error during download %s \n %s', url, err);
                         reject(err);
+                        return;
                     }
                     var ret = {
                         res: '',
@@ -16,94 +16,113 @@ class FileDownloader {
                     }
                     ret.res = res;
                     ret.data = data;
-                    console.log('syncdownload success:' + url);
                     resolve(ret);
                 })
             })
         };
 
-        return downloadPromise(url).then(function onfulfilled(value) {
-            var fs = require('fs');
-            try {
-                fs.writeFileSync(filepath, value.data.toString());
-                console.log('write success');
-            } catch (err) {
-                console.error('error during writing file %s \n %s', filepath, err);
-                throw err;
-            }
-        }).catch(function onrejected(err) {
-            console.error(err);
-        })
-    }
-    constructor() {
-        this.AsycSingleDownload = function (url, filepath, successcallback, failurecallback) {
-            var httpclient = require('urllib');
-            httpclient.request(url, function (err, data, res) {
-                if (err) {
-                    console.log('error during download %s \n %s', url, err);
-                    failurecallback(err, res);
-                };
+        var result = {
+            success: true,
+            err: null,
+            data: null
+        };
 
-                var fs = require('fs');
-                try {
-                    fs.writeFileSync(filepath, data.toString());
-                } catch (err) {
-                    console.log('error during writing file %s \n %s', filepath, err);
-                    failurecallback(err, res);
-                }
-                successcallback('success');
-            });
-        }
-
-        this.PromiseSingleDownload = function (url, filepath) {
-            return this._PromiseSingleDownload(url, filepath);
-        }
-
-        //PromiseMultiDownloads perform download remote resource simultainously.
-        //Args:
-        //  downloadtasks: array of download task. Each download task item is combination of url, filepath, finishstate, err
-        //return:
-        //  promise object so that caller can handle through then/reject
-        this.PromiseMultiDownloads = function (downloadtasks) {
-
-            function download(task, _PromiseSingleDownload) {
-                return function (downloaddone) {
-                    return _download(_task, _PromiseSingleDownload, downloaddone);
-                }
-            }
-
-            function _download(task, _PromiseSingleDownload, downloaddone) {
-                _PromiseSingleDownload(task.url, task.filepath)
-                    .then(function onfulfilled(ret) {
-                        task.finishstate = true;
-                        downloaddone(null, task);
-                    }).catch(function onrejected(err) {
-                        task.finishstate = false;
-                        task.err = err;
-                        downloaddone(err, task);
-                    });
-            }
-
-            var downloadfunctions = [];
-            //firstly, generate download functions array
-            for (var item in downloadtasks)
-                downloadfunctions.push(download(downloadtasks[item], this._PromiseSingleDownload));
-
-            return new Promise(function (resolve, reject) {
-                //using async module to
-                //  1. perform all download task from downloadtasks array
-                //  2. after all the downloads are finished, continue the work through then with 
-                var async = require('async');
-                async.parallelLimit(downloadfunctions, 5, function (err, results) {
-                    if (err == null)
-                        resolve(results);
-                    else
-                        reject(err, results);
+        return new Promise(function (resolve, reject) {
+                downloadPromise(url).then(function onfulfilled(value) {
+                    var fs = require('fs');
+                    try {
+                        fs.writeFileSync(filepath, value.data.toString());
+                        console.log('write success');
+                        result.success = true;
+                        result.err = null;
+                        result.data = null;
+                        resolve(result);
+                    } catch (err) { //catch error during file writing
+                        console.error('error during writing file %s \n %s', filepath, err);
+                        result.success = false;
+                        result.err = err;
+                        result.data = null;
+                        reject(err);
+                    }
+                }).catch(function onrejected(err) { //catch error during url download
+                    console.error('error during download url %s \n', url);
+                    result.success = false;
+                    result.err = err;
+                    result.data = null;
+                    reject(err);
                 })
             });
         }
+
+        constructor() {
+            this.AsycSingleDownload = function (url, filepath, successcallback, failurecallback) {
+                var httpclient = require('urllib');
+                httpclient.request(url, function (err, data, res) {
+                    if (err) {
+                        console.log('error during download %s \n %s', url, err);
+                        failurecallback(err, res);
+                    };
+
+                    var fs = require('fs');
+                    try {
+                        fs.writeFileSync(filepath, data.toString());
+                    } catch (err) {
+                        console.log('error during writing file %s \n %s', filepath, err);
+                        failurecallback(err, res);
+                    }
+                    successcallback('success');
+                });
+            }
+
+            this.PromiseSingleDownload = function (url, filepath) {
+                return this._PromiseSingleDownload(url, filepath);
+            }
+
+            //PromiseMultiDownloads perform download remote resource simultainously.
+            //Args:
+            //  downloadtasks: array of download task. Each download task item is combination of url, filepath, finishstate, err
+            //return:
+            //  promise object so that caller can handle through then/reject
+            this.PromiseMultiDownloads = function (downloadtasks, parallelsize) {
+
+                function download(task, _PromiseSingleDownload) {
+                    return function (downloaddone) {
+                        return _download(task, _PromiseSingleDownload, downloaddone);
+                    }
+                }
+
+                function _download(task, _PromiseSingleDownload, downloaddone) {
+                    return _PromiseSingleDownload(task.url, task.filepath)
+                        .then(function onfulfilled(ret) {
+                            task.finishstate = true;
+                            downloaddone(null, task);
+                        }, function onrejected(err) {
+                            task.finishstate = false;
+                            task.err = err;
+                            downloaddone(err, task);
+                        });
+                }
+
+                var downloadfunctions = [];
+                //firstly, generate download functions array
+                for (var item in downloadtasks)
+                    downloadfunctions.push(download(downloadtasks[item], this._PromiseSingleDownload));
+
+                return new Promise(function (resolve, reject) {
+                    //using async module to
+                    //  1. perform all download task from downloadtasks array
+                    //  2. after all the downloads are finished, continue the work through then with 
+                    var async = require('async');
+                    async.parallelLimit(downloadfunctions, parallelsize, function (err, results) {
+                        if (err == null)
+                            resolve(results);
+                        else
+                            reject(err);
+                    })
+                });
+            }
+        }
+
     }
 
-}
-
-module.exports = FileDownloader;
+    module.exports = FileDownloader;
--- test.js
+++ test.js
@@ -1,26 +1,58 @@
-const filedownloader = require ('./filedownloader');
+const filedownloader = require('./filedownloader');
 
 fdownload = new filedownloader();
 
-function downloadsuccess(value){
+function downloadsuccess(value) {
     console.log(value);
 }
-function downloadfailure(res, error){
+
+function downloadfailure(res, error) {
     console.error('error here');
     console.error(error);
 }
 //fdownload.AsycSingleDownload('http://www.baidu.com', './a.html', downloadsuccess, downloadfailure);
 
-//fdownload.PromiseSingleDownload('http://www.baidu.com', './a.html').then(function onfullfill(){console.log('PromiseDownload Finish')});
+var promise = fdownload.PromiseSingleDownload('https://www.baidu.com/', './a.html')
+promise.then(
+    function onfullfill() {
+        console.log('PromiseDownload Finish')
+    }
+).catch((err) => {
+    console.log(err);
+});
 var downloadtasks = [];
 downloadtasks.push({
-    url:'http://www.baidu.com',
-    filepath:'./1.html'
+    url: 'https://www.baidu.com',
+    filepath: './1.html'
 });
 
 downloadtasks.push({
-    url:'http://news.sina.com.cn',
-    filepath:'./2.html'
+    url: 'http://news.sina.com.cn',
+    filepath: './2.html'
 });
 
-fdownload.PromiseMultiDownloads(downloadtasks).then(function onfullfill(){console.log('PromiseMultiDownload Finish')});
+fdownload.PromiseMultiDownloads(downloadtasks, 1).then(
+    function onfullfill(result) {
+        console.log('PromiseMultiDownload Finish')
+    }).catch(
+    (err, results) => {
+        console.error(err)
+    });
+
+downloadtasks.push({
+    url: 'https://wwwwedew.baidu.com',
+    filepath: './3.html'
+});
+
+downloadtasks.push({
+    url: 'http://newsdewd.sina.com.cn',
+    filepath: './4.html'
+});
+
+fdownload.PromiseMultiDownloads(downloadtasks, 1).then(
+    function onfullfill(result) {
+        console.log('PromiseMultiDownload Finish')
+    }).catch(
+    (err, results) => {
+        console.error(err)
+    });
